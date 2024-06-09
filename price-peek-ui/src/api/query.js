343383bad9145a82c95e0a78f8557d204892304c
Jitendra Tiwari <jkjitendra725@gmail.com>
2024-06-09 23:36:16 +0530

    updated subdomain

--- price-peek-ui/src/api/query.js
+++ price-peek-ui/src/api/query.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const instance = axios.create({
-    baseURL: "https://ashutoshviramgama.com/",
+    baseURL: "https://pricepeek.ashutoshviramgama.com/",
     // withCredentials: true
 });
 
