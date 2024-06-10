e3e7076f3ecebf756bc4d76f247f183b27376f58
Jitendra Tiwari <jkjitendra725@gmail.com>
2024-06-11 00:32:41 +0530

    Update package.json scripts and Vite config for custom build setup and port configuration

--- price-peek-ui/vite.config.js
+++ price-peek-ui/vite.config.js
@@ -4,4 +4,15 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build:{
+    target:"esnext",
+    rollupOptions:{
+      input:{
+        popup: "index.html",
+      },
+      output:{
+        entryFileNames: "assets/[name].js"
+      }
+    },
+  },
 })
