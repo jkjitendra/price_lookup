c973a6481f9c6fb1d71b68187059dd0095d22bcf
Jitendra Tiwari <jkjitendra725@gmail.com>
2024-06-12 03:21:59 +0530

    added Logout confirmation

--- price-peek-ui/src/components/Logout.jsx
+++ price-peek-ui/src/components/Logout.jsx
@@ -4,28 +4,31 @@ import axios from "../api/query";
 
 const LOGOUT_URL = "/api/v1/auth/logout";
 
-
 const Logout = () => {
     const { logout } = useAuth();
     const navigate = useNavigate();
 
     const handleLogout = async () => {
-        try {
-            // Call the backend to clear the HttpOnly cookie
-            // await axios.post(LOGOUT_URL);
+        const userConfirmed = window.confirm('Are you sure you want to logout?');
+        
+        if (userConfirmed) {
+            try {
+                // Call the backend to clear the HttpOnly cookie
+                // await axios.post(LOGOUT_URL);
 
-            // Clear any client-side authentication state
-            logout();
+                // Clear any client-side authentication state
+                logout();
 
-            // Redirect to the login page
-            navigate('/login');
-        } catch (error) {
-            console.error('Error logging out', error);
+                // Redirect to the login page
+                navigate('/login');
+            } catch (error) {
+                console.error('Error logging out', error);
+            }
         }
     };
 
     return (
-        <button onClick={handleLogout}>Logout</button>
+        <button onClick={handleLogout} className='logout-btn'>Logout</button>
     );
 };
 
