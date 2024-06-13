85c928d646742535662e6f66c8886c3cd6d30708
Jitendra Tiwari <jkjitendra725@gmail.com>
2024-06-14 00:05:44 +0530

    feat: Implement search and filter functionality in Search component

---
+++ price-peek-ui/src/components/Search.jsx
@@ -0,0 +1,38 @@
+import React, { useEffect, useState } from 'react';
+import { useSelector } from 'react-redux';
+import DataTable from './DataTable'; 
+import '../assets/styles/Search.css';
+
+const Search = ({products}) => {
+  const [searchTerm, setSearchTerm] = useState('');
+  const [filteredProducts, setFilteredProducts] = useState(products);
+  // const products = useSelector(state => state.products.products);
+
+  useEffect(() => {
+    setFilteredProducts(
+      products.filter(product =>
+        product.url?.toLowerCase().includes(searchTerm.toLowerCase())
+      )
+    );
+  }, [searchTerm, products]);
+
+  console.log('products in search', products);
+  console.log('filteredProducts in search', filteredProducts);
+  
+  return (
+    <div className="search-wrapper">
+      <div className="search-bar">
+        <input
+          type="text"
+          placeholder="Search Product Name"
+          value={searchTerm}
+          onChange={e => setSearchTerm(e.target.value)}
+          className="search-input"
+        />
+      </div>
+      <DataTable products={filteredProducts} />
+    </div>
+  );
+};
+
+export default Search;
