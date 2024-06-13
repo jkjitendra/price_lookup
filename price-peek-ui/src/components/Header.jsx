c9446d6719d3d3d3fe40eadea9815785b8606c91
Jitendra Tiwari <jkjitendra725@gmail.com>
2024-06-13 23:57:14 +0530

    added Header Component with logo and name of extension

---
+++ price-peek-ui/src/components/Header.jsx
@@ -0,0 +1,15 @@
+// src/components/Header.jsx
+import React from 'react';
+import logo from '../assets/images/PricePeek.png';
+import '../assets/styles/Header.css';
+
+const Header = () => {
+  return (
+    <header className="header">
+      <img src={logo} alt="Price Peek Logo" className="logo" />
+      <h1 className="app-name">Price Peek</h1>
+    </header>
+  );
+};
+
+export default Header;
