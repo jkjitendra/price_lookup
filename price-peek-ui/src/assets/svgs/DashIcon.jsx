dd561dc9a44214d9f3a81eb3e7a31092e5c594de
Jitendra Tiwari <jkjitendra725@gmail.com>
2024-06-19 00:13:24 +0530

    Added DashIcon svg component

---
+++ price-peek-ui/src/assets/svgs/DashIcon.jsx
@@ -0,0 +1,12 @@
+import React from'react';
+
+const DashIcon = ({ onClick}) => {
+    return (
+        <div className='flex items-center justify-center'>
+            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512" width='24px'><path d="M432 256c0 17.7-14.3 32-32 32L48 288c-17.7 0-32-14.3-32-32s14.3-32 32-32l352 0c17.7 0 32 14.3 32 32z"/>
+            </svg>
+        </div>
+    );
+}
+
+export default DashIcon;
