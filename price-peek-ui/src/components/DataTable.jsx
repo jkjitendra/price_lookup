aba35fcd6ae054bdbbdc209111db479c8abeda6f
Jitendra Tiwari <jkjitendra725@gmail.com>
2024-06-20 01:32:10 +0530

    added EditProduct & DeleteProduct Feature

--- price-peek-ui/src/components/DataTable.jsx
+++ price-peek-ui/src/components/DataTable.jsx
@@ -1,13 +1,103 @@
-import React from 'react';
+import React, { useState, useEffect, useRef } from 'react';
 import '../assets/styles/DataTable.css';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import EditIcon from '../assets/svgs/EditIcon';
 import DeleteIcon from '../assets/svgs/DeleteIcon';
+import SaveIcon from '../assets/svgs/SaveIcon';
+import axios from '../api/query';
+import extractProductId from '../utils/extractFromURL';
+import DashIcon from '../assets/svgs/DashIcon';
 
-const DataTable = ({ products }) => {
+const DELETE_URL = '/product';
+const UPDATE_URL = '/update-product';
+
+const DataTable = ({ products, setProducts }) => {
+
+  const [editingProductId, setEditingProductId] = useState(null);
+  const [newTargetPrice, setNewTargetPrice] = useState('');
+  const editFieldRef = useRef(null);
+
   const calculateLowestPrice = (priceList) => {
-    return priceList.length ? Math.min(...priceList) : 'N/A';
+    return priceList && priceList.length ? `₹${Math.min(...priceList)}` : <DashIcon />;
   };
+
+  const handleEdit = (product) => {
+    setEditingProductId(product.id);
+    setNewTargetPrice(product.target_price);
+  };
+
+  const handleSave = async (product) => {
+    const accessToken = localStorage.getItem('accessToken');
+    try {
+      const response = await axios.put(`${UPDATE_URL}/${product.id}`, {
+        product_link: product.url,
+        target_price: newTargetPrice,
+        product_platform: product.platform
+      }, {
+        headers: {
+          'Authorization': `Bearer ${accessToken}`,
+        },
+      });
+
+      if (response.data.success) {
+        setProducts((prevProducts) =>
+          prevProducts.map((p) =>
+            p.id === product.id ? { ...p, target_price: response.data.changes.target_price } : p
+          )
+        );
+        setEditingProductId(null);
+      } else {
+        console.error('Failed to update the product');
+        setEditingProductId(null);
+      }
+    } catch (error) {
+      console.error('There was an error updating the product!', error);
+      setEditingProductId(null);
+    }
+  };
+
+  const handleDelete = async (product) => {
+    const productId = product.id;
+    const userConfirmed = window.confirm(`Delete product: ${product.name}?`);
+    if (userConfirmed) {
+        const accessToken = localStorage.getItem('accessToken');
+        try {
+          const response = await axios.delete(`${DELETE_URL}/${productId}`, {
+            headers: {
+              'Authorization': `Bearer ${accessToken}`,
+            },
+          });
+    
+          if (response.data.status === 'Product deleted successfully') {
+            setProducts((prevProducts) => prevProducts.filter(product => product.id !== productId));
+          } else {
+            console.error('Failed to delete the product');
+          }
+        } catch (error) {
+          console.error('There was an error deleting the product!', error);
+        }
+    }
+  };
+
+  const goToExternalURL = (url) => {
+    window.open(url, '_blank');
+  };
+
+  const handleClickOutside = (event) => {
+    if (editFieldRef.current && !editFieldRef.current.contains(event.target)) {
+      setEditingProductId(null);
+    }
+  };
+
+  useEffect(() => {
+    document.addEventListener('mousedown', handleClickOutside);
+    return () => {
+      document.removeEventListener('mousedown', handleClickOutside);
+    };
+  }, []);
+
+  useEffect(() => {
+    console.log('products recieved ', products);
+  }, [products]);
 
   console.log('products recieved ', products);
   return (
@@ -16,9 +106,9 @@ const DataTable = ({ products }) => {
         <thead>
           <tr>
             <th></th>
-            <th>Image</th>
+            {/* <th>Image</th> */}
             <th>Name</th>
-            <th>Current Price</th>
+            {/* <th>Current Price</th> */}
             <th>Lowest Price</th>
             <th>Target Price</th>
             <th>Action</th>
@@ -37,18 +127,34 @@ const DataTable = ({ products }) => {
         <tbody>
           {products.map((product, index) => (
             <tr key={product.id}>
-              <td>{index+1}</td>
-              <td><img src={product?.image} alt={product?.name} className="product-image" /></td>
-              <td>{product?.name}</td>
-              <td>₹{product?.currentPrice}</td>
-              <td>₹{calculateLowestPrice(product?.price_list)}</td>
-              <td>₹{product?.target_price}</td>
+              <td style={{color: 'blue', fontWeight: 'bold'}}>{index+1}</td>
+              {/* <td><img src={product?.image} alt={product?.name} className="product-image" /></td> */}
+              <td onClick={() => goToExternalURL(product.url)} style={{ cursor: 'pointer', textDecoration: 'underline', color: 'blue' }}>{product?.name}</td>
+              {/* <td onClick={() => goToExternalURL(product.url)} style={{ cursor: 'pointer', textDecoration: 'underline', color: 'blue' }}>
+                {extractProductId(product.url)}
+              </td> */}
+              {/* <td>₹{product?.price}</td> */}
+              <td>{calculateLowestPrice(product?.price_list)}</td>
               <td>
-                {/* <button className="action-btn edit-btn">Edit</button> */}
-                {/* <button className="action-btn delete-btn">Delete</button> */}
+                {editingProductId === product.id ? (
+                  <input
+                    type="number"
+                    value={newTargetPrice}
+                    onChange={(e) => setNewTargetPrice(e.target.value)}
+                    className="editable-input"
+                  />
+                ) : (
+                  `₹${product?.target_price}`
+                )}
+              </td>
+              <td>
                 <div className='svg-container'>
-                  <EditIcon />
-                  <DeleteIcon />
+                  {editingProductId === product.id ? (
+                    <SaveIcon onClick={() => handleSave(product)} />
+                  ) : (
+                    <EditIcon onClick={() => handleEdit(product)} />
+                  )}
+                  <DeleteIcon onClick={() => handleDelete(product)} />
                 </div>
               </td>
             </tr>
