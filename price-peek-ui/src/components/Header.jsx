e46a1b152561c5897239ca98961e0ac88b556ed4
Jitendra Tiwari <jkjitendra725@gmail.com>
2025-03-19 23:56:24 +0530

    feat: Integrate SectionList and UserMenu components into Header

--- price-peek-ui/src/components/Header.jsx
+++ price-peek-ui/src/components/Header.jsx
@@ -1,11 +1,23 @@
+import { useState } from 'react';
 import logo from '../assets/images/PricePeek.png';
+import UserMenu from './UserMenu';
+import SectionList from './SectionList';
+import ProductsList from './ProductsList';
+import AddProduct from './AddProduct';
+
 import '../assets/styles/Header.css';
 
 const Header = () => {
+  const [activeSection, setActiveSection] = useState('productsList');
   return (
     <header className="header">
-      <img src={logo} alt="Price Peek Logo" className="logo" />
-      <h1 className="app-name">Price Peek</h1>
+      {/* <img src={logo} alt="Price Peek Logo" className="logo" />
+      <SectionList setActiveSection={setActiveSection} /> */}
+      <div className="header-left">
+        <img src={logo} alt="Price Peek Logo" className="logo" />
+        <SectionList setActiveSection={setActiveSection} />
+      </div>
+      <UserMenu />
     </header>
   );
 };
