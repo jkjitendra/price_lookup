d72a8b516e7f7e13f8e6b302b3347f223f43aef0
Jitendra Tiwari <jkjitendra725@gmail.com>
2025-03-21 23:39:23 +0530

    Refactor Header component to accept setActiveSection as a prop for better state management

--- price-peek-ui/src/components/Header.jsx
+++ price-peek-ui/src/components/Header.jsx
@@ -2,17 +2,12 @@ import { useState } from 'react';
 import logo from '../assets/images/PricePeek.png';
 import UserMenu from './UserMenu';
 import SectionList from './SectionList';
-import ProductsList from './ProductsList';
-import AddProduct from './AddProduct';
 
 import '../assets/styles/Header.css';
 
-const Header = () => {
-  const [activeSection, setActiveSection] = useState('productsList');
+const Header = ({setActiveSection}) => {
   return (
     <header className="header">
-      {/* <img src={logo} alt="Price Peek Logo" className="logo" />
-      <SectionList setActiveSection={setActiveSection} /> */}
       <div className="header-left">
         <img src={logo} alt="Price Peek Logo" className="logo" />
         <SectionList setActiveSection={setActiveSection} />
